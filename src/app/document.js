0957e655698edc3d965302c11c52fc1aec4db70b
Eljuan25 <147083751+Eljuan25@users.noreply.github.com>
2025-01-20 16:46:50 -0600

    Complete project enhancements: (#1)
    - Added footer component with location and contact details.
    - Integrated Google Maps iframe for clinic location.
    - Improved overall styling (circular image borders, layout adjustments, and font styles).
    - Enhanced description section with detailed text and aligned layout.
    - Adjusted spacing for header and main content.
    - Updated CSS module for better responsiveness and user experience.

---
+++ src/app/document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          {/* Preconectar para mejorar el rendimiento de la fuente */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          {/* Importar la fuente de Google Fonts */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playwright+IN:wght@100..400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
